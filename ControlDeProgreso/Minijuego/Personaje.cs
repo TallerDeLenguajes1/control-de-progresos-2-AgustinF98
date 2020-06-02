c2f1204c2dd403502643adef15b2299d80a9631d
Agustin <agus_gymnas2009@hotmail.com>
2020-06-02 16:04:05 -0300

    Agrego caracteristicas

---
+++ ControlDeProgreso/Minijuego/Personaje.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minijuego
+{
+
+    enum Caracteristicas
+    {
+        velocidad,
+        destreza,
+        fuerza,
+        nivel,
+        armadura
+    }
+
+    public class Personaje
+    {
+        Caracteristicas caracteristicas;
+    }
+}
---
+++ ControlDeProgreso/Minijuego/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Minijuego
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
